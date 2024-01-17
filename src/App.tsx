9cc42ceae437c0be96e28f484ad4deffabf39dcf
joseph <limjoseph2004@gmail.com>
2024-01-17 21:48:13 +1100

    Install prettier for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Test from "./components/Test";
 
 function App() {
-	return (
-		<>
-			<Test />
-		</>
-	);
+  const [test, setText] = useState("default text");
+  return (
+    <div className="mx-auto flex w-[20rem] flex-col justify-center gap-5">
+      <Test />
+      <p>{test}</p>
+    </div>
+  );
 }
 
 export default App;
