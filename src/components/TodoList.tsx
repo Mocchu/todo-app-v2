f475fc70e6dd2c297fa8d05eeafc3a88d0a7b901
joseph <limjoseph2004@gmail.com>
2024-01-26 20:05:40 +1100

    Remove console.log

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -22,7 +22,6 @@ export default function TodoList({
   const [openNewTodoSheet, setOpenEditTodoSheet] = useState(false);
 
   const project = projects.find((project) => project.key === activeProjectKey);
-  console.log(project);
 
   function handleRowClick(key) {
     setselectedTodoKey(key);
